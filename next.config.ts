f21b4457cf75e1349270d41668a01eaae791f64c
manishtmtmt <manisht.mt.mt@gmail.com>
2025-06-15 09:14:07 +0530

    fix: Update ESLint configuration to ignore errors during builds

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Completely disables ESLint during builds
   },
+  env: {
+    NEXT_PUBLIC_CLERK_JS_VARIANT: "static", // Ensures stable version of ClerkJS
+  },
 };
 
 export default nextConfig;
