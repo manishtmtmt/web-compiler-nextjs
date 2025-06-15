8742f7c4bcd6a330d463292127779f2785b6f9db
manishtmtmt <manisht.mt.mt@gmail.com>
2025-06-15 09:42:08 +0530

    fix: Add dynamic prop to ClerkProvider for improved functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      dynamic
+    >
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
